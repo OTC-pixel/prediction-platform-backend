693ebc60887dd6200bf1112afce0847ac4e0b488
Tumukunde Deogratious <tumukunde012@gmail.com>
2025-08-30 04:36:29 +0300

    Update CORS and ping endpoint for Render deployment

--- app.py
+++ app.py
@@ -78,5 +78,6 @@ def teardown_db(exception):
 # ------------------------------
 if __name__ == "__main__":
     from waitress import serve
-    # Production-ready server
-    serve(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host="0.0.0.0", port=port)
+
